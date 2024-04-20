147c51d1b143b20c90015e4a9d1eb3831cb0decd
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-04-20 13:43:08 -0700

    cursor animation on mousedown

--- cursor.js
+++ cursor.js
@@ -1,6 +1,14 @@
 const cursorDot = document.querySelector("[data-cursor-dot]");
 const cursorOutline = document.querySelector("[data-cursor-outline]");
 
+const outlineClickSize = 25;
+const outlineSize = 30;
+const clickDuration = 300;
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 window.addEventListener("mousemove", function (e) {
   const posX = e.clientX;
   const posY = e.clientY;
@@ -16,6 +24,26 @@ window.addEventListener("mousemove", function (e) {
       left: `${posX}px`,
       top: `${posY}px`,
     },
-    { duration: 10, fill: "forwards" }
+    { duration: 150, fill: "forwards" }
   );
+});
+
+window.addEventListener("mousedown", function (e) {
+  console.log("HEY 1!");
+  cursorOutline.animate(
+    {
+      width: `${outlineClickSize}px`,
+      height: `${outlineClickSize}px`,
+    },
+    { duration: 400, easing: "ease" }
+  );
+  sleep(200).then(() => {
+    cursorOutline.animate(
+      {
+        width: `${outlineSize}px`,
+        height: `${outlineSize}px`,
+      },
+      { duration: 500, easing: "ease" }
+    );
+  });
 });
