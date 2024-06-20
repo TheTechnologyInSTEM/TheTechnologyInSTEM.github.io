fe31f3c324545544e1b501e652a30d7df9be52ac
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-06-20 10:10:04 -0700

    removed react-bootstrap

---
+++ tinstemsite/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button() {
+  return (
+    <button type="button" className="btn btn-primary">
+      Base Class
+    </button>
+  );
+}
+
+export default Button;
