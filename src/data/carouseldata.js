707d680c689e07393e3bee7e8c0178fbbdc8dfe3
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-06-20 19:53:35 -0700

    image carousel completed

--- src/data/carouseldata.js
+++ src/data/carouseldata.js
@@ -1,22 +1,13 @@
+import picture1 from "../assets/picture1.jpg";
+import picture2 from "../assets/picture2.jpg";
+import picture3 from "../assets/picture3.jpg";
+import picture4 from "../assets/picture4.jpg";
+import picture5 from "../assets/picture5.jpg";
+
 export const slides = [
-  {
-    url: "https://images.unsplash.com/photo-1553649033-3fbc8d0fa3cb?ixlib=rb-4.0.3",
-    alt: "Card 1",
-  },
-  {
-    url: "https://wallpapers.com/images/hd/1920-x-1080-hd-1qq8r4pnn8cmcew4.jpg",
-    alt: "Card 2",
-  },
-  {
-    url: "https://images.pexels.com/photos/2749481/pexels-photo-2749481.jpeg?cs=srgb&dl=pexels-nicole-avagliano-1132392-2749481.jpg&fm=jpg",
-    alt: "Card 3",
-  },
-  {
-    url: "https://preview.redd.it/u105ro5rg8o31.jpg?auto=webp&s=762f11d489021e560e760ea13702fad955b291f1",
-    alt: "Card 4",
-  },
-  {
-    url: "https://wallpapers.com/images/hd/1920-x-1080-hd-2kph8n73fpmvddj1.jpg",
-    alt: "Card 5",
-  },
+  { url: picture1, alt: "Picture1" },
+  { url: picture2, alt: "Picture 2" },
+  { url: picture3, alt: "Picture 3" },
+  { url: picture4, alt: "Picture 4" },
+  { url: picture5, alt: "Picture 5" },
 ];
