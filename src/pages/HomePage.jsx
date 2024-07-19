c02e6a7fc923fef8d769a8483cff256a78f5fef0
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-07-18 20:44:46 -0700

    hard reset

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,9 @@
 import ImageCarousel from "../components/ImageCarousel";
-import { slides } from "../data/carouseldata";
 
 export const HomePage = () => {
-  const containerStyles = {
-    maxWidth: "1200px",
-    width: "100%",
-    aspectRatio: "10 / 6",
-    margin: "0 auto",
-  };
-
   return (
     <>
-      <h1>Hello!</h1>
-      <div style={containerStyles}>
-        <ImageCarousel slides={slides}></ImageCarousel>
-      </div>
+      <ImageCarousel slides={slides}></ImageCarousel>
     </>
   );
 };
