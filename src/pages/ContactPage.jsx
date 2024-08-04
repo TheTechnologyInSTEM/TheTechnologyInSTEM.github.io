d43056209562e6821100d8dbe3a6b0272261b9f8
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-08-04 12:53:50 -0700

    contact page finished

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -7,23 +7,8 @@ export const ContactPage = () => {
   return (
     <>
       <Header classes="pt-3 px-3"></Header>
-      <div className="container-fluid mt-5 pt-3">
-        <div class="row text-center py-3">
-          <h1>
-            <TypedComponent
-              strings={contacttext}
-              doesLoop={false}
-            ></TypedComponent>
-          </h1>
-        </div>
-
+      <div className="container-fluid pt-8">
         <div className="row mx-7">
-          <div className="col-5">
-            <img
-              src="https://i.postimg.cc/j2Rts40h/Copy-of-The-T-IN-STEM.png"
-              className="img-fluid"
-            ></img>
-          </div>
           <div className="col text-center mx-10">
             <div className="my-auto">
               <h1 className="fw-bold">Get In Touch</h1>
@@ -54,7 +39,6 @@ export const ContactPage = () => {
           </div>
         </div>
       </div>
-      <Footer></Footer>
     </>
   );
 };
