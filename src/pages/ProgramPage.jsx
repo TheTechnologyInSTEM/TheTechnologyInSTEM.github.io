8a3ab2f5fcf46b1543876c592eb245a75cc61ad8
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-08-04 19:18:34 -0700

    contact page format complete

--- src/pages/ProgramPage.jsx
+++ src/pages/ProgramPage.jsx
@@ -1,9 +1,77 @@
-import Header from "../components/Header"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export const ProgramPage = () => {
   return (
     <>
       <Header classes="pt-3 px-3" text="Programs"></Header>
+      <div className="container-fluid pt-6">
+        <div className="row text-center">
+          <h1 className="fw-bold">Community Impact</h1>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img
+              className="img-fluid"
+              src="https://i.postimg.cc/zGYqPxHP/unnamed-10.jpg"
+            />
+          </div>
+          <div className="col">
+            <img
+              className="img-fluid"
+              src="https://i.postimg.cc/1RNtCnYX/unnamed-16.jpg"
+            />
+          </div>
+          <div className="col">
+            <img
+              className="img-fluid"
+              src="https://i.postimg.cc/cCYLr28w/unnamed-17.jpg"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img
+              className="img-fluid"
+              src="https://i.postimg.cc/MTnT8v6s/unnamed-14.jpg"
+            />
+          </div>
+          <div className="col">
+            <div className="m-5 text-center">
+              <h1 className="fw-bold">The T in STEM</h1>
+              <p className="fs-3">write something here</p>
+            </div>
+          </div>
+          <div className="col">
+            <img
+              className="img-fluid"
+              src="https://i.postimg.cc/tT8gRp79/unnamed-15.jpg"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img
+              className="img-fluid"
+              src="https://i.postimg.cc/xC8Qb7Bq/unnamed-13.jpg"
+            />
+          </div>
+          <div className="col">
+            <img
+              className="img-fluid"
+              src="https://i.postimg.cc/02w8fTjw/unnamed-6.jpg"
+            />
+          </div>
+          <div className="col">
+            <img
+              className="img-fluid"
+              src="https://i.postimg.cc/hGScKhrL/unnamed-7.jpg"
+            />
+          </div>
+        </div>
+      </div>
+
+      <Footer></Footer>
     </>
   );
 };
